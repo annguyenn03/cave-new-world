3621ba3454f53f21b17c42031c6a8b7eb1ef590f
An <anthunguyen03@gmail.com>
2025-02-28 22:34:02 -0800

    Edit Camera Location

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import numpy as np
 mi.set_variant('cuda_ad_rgb')
 
 #Load Scene
-scene = mi.load_file('asset/cave_scene.xml')
+scene = mi.load_file('asset/cave_scene_cut.xml')
 image = mi.render(scene, spp= 1024)
 bitmap = mi.Bitmap(image)
 image = np.array(bitmap)
 
-plt.imshow(image)
+# plt.imshow(image)
+# plt.imshow(image ** (1.0 / 2.2)); # approximate sRGB tonemapping
+plt.imshow(image ** 0.3); # approximate sRGB tonemapping
 plt.axis("off")
 plt.show()
