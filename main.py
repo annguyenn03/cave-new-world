1463ca835d30be82f47d77b53403dbe81bac5b3f
An <anthunguyen03@gmail.com>
2025-04-05 13:52:30 -0700

    Adjust medium's value + Post-processing

--- main.py
+++ main.py
@@ -4,14 +4,13 @@ import numpy as np
 
 mi.set_variant('cuda_ad_rgb')
 
-#Load Scene
+#Load Scene and Render
 scene = mi.load_file('asset/cave_scene_cut.xml')
 image = mi.render(scene, spp= 1024)
 bitmap = mi.Bitmap(image)
 image = np.array(bitmap)
 
-# plt.imshow(image)
+#Show the output
 plt.imshow(image ** (1.0 / 2.2)); # approximate sRGB tonemapping
-# plt.imshow(image ** 0.3); # approximate sRGB tonemapping
 plt.axis("off")
 plt.show()
